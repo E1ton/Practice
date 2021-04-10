78d3d808a4a40752c66af350f8d489038cefce00
E1ton <2284619262@qq.om>
2021-04-10 19:05:40 +0930

    dssd

--- TextQuery.h
+++ TextQuery.h
@@ -6,7 +6,8 @@
 #include <map>
 #include <set>
 
-class QueryResult;
+using line_no = std::vector<std::string>::size_type;
+
 class TextQuery {
 public:
     using line_no = std::vector<std::string>::size_type;
@@ -16,4 +17,17 @@ private:
     std::shared_ptr<std::vector<std::string>> file;
     std::map<std::string, std::shared_ptr<std::set<line_no>>> wm;
 };
+
+class QueryResult {
+friend std::ostream& print(std::ostream&, const QueryResult&);
+public: 
+    QueryResult(std::string s,
+    std::shared_ptr<std::set<line_no>> p,
+    std::shared_ptr<std::vector<std::string>> f) : sought(s), lines(p), file(f) {}    
+private:
+    std::string sought;
+    std::shared_ptr<std::set<line_no>> lines; 
+    std::shared_ptr<std::vector<std::string>> file;
+};
+
 #endif
