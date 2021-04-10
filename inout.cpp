34c9848a32cc2c661b2866c9b70005a528333f92
E1ton <2284619262@qq.om>
2021-04-10 21:07:50 +0930

    inout

---
+++ inout.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    int c;
+    cin >> a >> b;
+    cin >> c;
+    cout << c << endl;
+    return 0;
+}
