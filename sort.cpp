cd291250cd703dada61e6462ebf3a138e2a6d160
E1ton <2284619262@qq.om>
2021-04-12 15:19:10 +0930

    shellsort

--- sort.cpp
+++ sort.cpp
@@ -7,18 +7,6 @@ void swap(int *a, int *b) {
     *b = temp;    
 }
 //if n not provided we can use 'sizeof(array)/sizeof(array[0])' to calculate size
-void insertionSort(int array[], int n) {
-    for(int i=0; i<n; i++) {
-        int j = i - 1;
-        int curr = array[i];
-        while(j >=0 && array[j] > curr) {
-            array[j+1] = array[j];
-            j--;
-        }
-        array[j+1] = curr;
-    }
-}
-
 void bubbleSort(int array[], int n) {
     bool sorted = true;
     do {
@@ -32,9 +20,43 @@ void bubbleSort(int array[], int n) {
     } while(sorted == false);
 }
 
+void selectionSort(int array[], int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            if(array[j] < array[i]) swap(&array[i], &array[j]);
+        }
+    }
+}
+
+void insertionSort(int array[], int n) {
+    for(int i=0; i<n; i++) {
+        int j = i - 1;
+        int curr = array[i];
+        while(j >= 0 && array[j] > curr) {
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = curr;
+    }
+}
+
+void shellSort(int array[], int n) {
+    for(int gap = n/2; gap>0; gap /= 2) {
+        for(int i=0; i<n; i+=1) {
+            int j = i-gap;
+            int curr = array[i];
+            while(j >= 0 && array[j] > curr) {
+                array[j+gap] = array[j];
+                j -= gap; 
+            }
+            array[j+gap] = curr;
+        }
+    }
+}
+
 int main() {
     int test[] = {3,4,6,77,2,5,3,1};
-    bubbleSort(test, 8);
+    shellSort(test, 8);
     for(int i=0; i<8; i++) {
         cout << test[i] << ' ';
     }
